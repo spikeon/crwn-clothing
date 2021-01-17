bca1be9d5b2f60a9fd975a5e26629a085b99cbb0
Mike Flynn <spikeon@gmail.com>
2021-01-16 23:57:04 -0800

    Adding email and password sign up

--- src/App.js
+++ src/App.js
@@ -5,22 +5,32 @@ import {Route, Switch} from 'react-router-dom';
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {auth} from "./firebase/firebase.utils";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 
 class App extends React.Component {
 
     constructor(props, context) {
         super(props, context);
         this.state = {
-            currentUser : null
+            currentUser: null
         };
     }
 
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser : user});
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+                userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    });
+                });
+            } else this.setState({currentUser: null});
         })
     }
 
@@ -31,7 +41,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Header currentUser={this.state.currentUser} />
+                <Header currentUser={this.state.currentUser}/>
                 <Switch>
                     <Route exact path="/" component={HomePage}/>
                     <Route exact path="/shop" component={ShopPage}/>
