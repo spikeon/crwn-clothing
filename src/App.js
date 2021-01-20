26cdb1e210618fd6138533f372c89dbfa031538e
Mike Flynn <spikeon@gmail.com>
2021-01-20 14:06:18 -0800

    Adding redirect on sign in page if logged in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 import {HomePage} from "./pages/homepage/homepage.component";
-import {Route, Switch} from 'react-router-dom';
+import {Redirect, Route, Switch} from 'react-router-dom';
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
@@ -24,8 +24,7 @@ class App extends React.Component {
                         ...snapShot.data()
                     });
                 });
-            }
-            else setCurrentUser(null);
+            } else setCurrentUser(null);
         })
     }
 
@@ -40,15 +39,19 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path="/" component={HomePage}/>
                     <Route exact path="/shop" component={ShopPage}/>
-                    <Route exact path="/signIn" component={SignInAndSignUp}/>
+                    <Route exact path="/signIn" render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUp/>)}/>
                 </Switch>
             </div>
         );
     }
 }
 
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+});
+
 const mapDispatchToProps = (dispatch) => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
