44f5f8927eeda04da7c936fe96198900bdefd0ac
Mike Flynn <spikeon@gmail.com>
2021-01-20 15:38:02 -0800

    Grouping cart items

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import CustomButton from "../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
-import {connect} from "react-redux";
 
 const CartDropdown = () => (
     <div className='cart-dropdown'>
