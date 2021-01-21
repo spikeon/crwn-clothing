af785a44cdb35cf4e78f546f865012b1794274c6
Mike Flynn <spikeon@gmail.com>
2021-01-21 12:34:49 -0800

    Code Cleanup

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -7,27 +7,22 @@ const CheckoutItem = ({item, clearItem, removeItem, addItem}) => {
     const {name, quantity, price, imageUrl} = item;
     return (
         <div className='checkout-item'>
+
             <div className='image-container'>
                 <img src={imageUrl} alt='item'/>
             </div>
-            <span className='name'>
-                {name}
-            </span>
+
+            <span className='name'>{name}</span>
+
             <div className='quantity'>
-                <div className='arrow'  onClick={() => removeItem(item)}>
-                    &#10094;
-                </div>
+                <div className='arrow' onClick={() => removeItem(item)}>&#10094;</div>
                 <span className='value'>{quantity}</span>
-                <div className='arrow' onClick={() => addItem(item)}>
-                    &#10095;
-                </div>
+                <div className='arrow' onClick={() => addItem(item)}>&#10095;</div>
             </div>
-            <span className='price'>
-                ${quantity * price}
-            </span>
-            <div className='remove-button' onClick={() => clearItem(item)}>
-                &#10005;
-            </div>
+
+            <span className='price'>${price}</span>
+
+            <div className='remove-button' onClick={() => clearItem(item)}>&#10005;</div>
         </div>
     );
 };
