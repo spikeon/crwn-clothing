c57467105accde2442e7696a4eb5037eafa2c990
Mike Flynn <spikeon@gmail.com>
2021-01-21 12:55:20 -0800

    Adding react persist

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import store from "./redux/store";
+import {persistor, store} from "./redux/store";
+import {PersistGate} from "redux-persist/integration/react"
+
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App/>
+            <PersistGate persistor={persistor}>
+                <App/>
+            </PersistGate>
         </BrowserRouter>
     </Provider>,
     document.getElementById('root')
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,22 @@
 import {combineReducers} from "redux";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import storage from "redux-persist/lib/storage"
+import {persistReducer} from "redux-persist";
 
 const reducers = {
     user: userReducer,
     cart: cartReducer
 };
 
-export default combineReducers(reducers);
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    whitelist: [
+        'cart'
+    ]
+};
+
+const rootReducer = combineReducers(reducers);
+
+export default persistReducer(persistConfig, rootReducer);
