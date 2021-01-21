0d02ff5329bab947e52260054c0117c022cb8b1a
Mike Flynn <spikeon@gmail.com>
2021-01-20 18:19:58 -0800

    Adding reselect library Using reselect in cart icon and dropdown

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,8 +1,10 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
+
     const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
+
     if(existingCartItem) {
         return cartItems.map(cartItem =>
-            cartItem.id === cartItemToAdd ? {...cartItem, quantity : cartItem.quantity + 1} : cartItem
+            cartItem.id === cartItemToAdd.id ? {...cartItem, quantity : cartItem.quantity + 1} : cartItem
         )
     }
 
