82f0dcf08755b9d4a14151713b0735993a62ac6c
Mike Flynn <spikeon@gmail.com>
2021-01-21 22:41:40 -0800

    Adding nested routing for Shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import "./shop.styles.scss";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-import {connect} from "react-redux";
-import {createStructuredSelector} from "reselect";
-import {selectCollections} from "../../redux/shop/shop.selector";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import {Route, Switch} from "react-router-dom";
 
-const ShopPage = ({collections}) => (
+const ShopPage = ({match}) => (
     <div className="shop-page">
-        {
-            collections.map(({id, title, items}) => (
-                <CollectionPreview key={id} title={title} items={items} />
-            ))
-        }
+        <Switch>
+            <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+        </Switch>
     </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-    collections : selectCollections
-})
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
